e94ad7c0bd82b314ab82ef90f7d71644fee7c868
Shilpa Gopal <shlpgopal@gmail.com>
2024-06-28 21:49:57 -0400

    new file:   DjangoDevJournal/static/about.css

--- DjangoDevJournal/DjangoDevJournal/views.py
+++ DjangoDevJournal/DjangoDevJournal/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, welcome to Home page")
+    # return HttpResponse("Hello, welcome to Home page")
+    return render(request, 'websites/index.html')
 
 def about(request):
-    return HttpResponse("Hello, welcome to About page")
+    return render(request, 'websites/about.html')
 
 def contact(request):
     return HttpResponse("Hello, welcome to Contact page")
